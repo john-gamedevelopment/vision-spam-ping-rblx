e8feff18309fed67284118224b1f22e3979f16f0
john <hugeaxel043@gmail.com>
2025-03-04 18:02:02 -0500

    Update vision spam ping.lua

--- vision spam ping.lua
+++ vision spam ping.lua
@@ -5,12 +5,6 @@
 / /_/ / / /  __/  (__  ) /_/ /_/ / /  / /_/ / /_/ /  __/  / /_/ / / / / / /_  
 \__/_/ /_/\___/  /____/\__/\____/_/   \__,_/\__, /\___/   \__,_/_/ /_/_/\__/  
                                            /____/                             
-.gg/HAJYZ4WemA
-.gg/HAJYZ4WemA
-.gg/HAJYZ4WemA
-.gg/HAJYZ4WemA
-.gg/HAJYZ4WemA
-
 much love and enjoy <3 - john
 ]]--
 
